28f2651e8ea430351142170efe42399b57576c29
Pierre Bellec <pierre.bellec@gmail.com>
2017-01-11 16:15:42 -0500

    Support for multiple runs in SPM generation.

--- hcp_brick_fmridesign.m
+++ hcp_brick_fmridesign.m
@@ -3,9 +3,8 @@ function [in,out,opt] = hcp_brick_fmridesign(in,out,opt)
 %
 % SYNTAX: [IN,OUT,OPT] = HCP_BRICK_FMRIDESIGN(IN,OUT,OPT)
 %
-% IN.FMRI (string) the file name of a 4D volume
-
-% IN.ONSET (string, default '') a csv onset file
+% IN.FMRI (string or cell of strings) the file name of a 4D volume
+% IN.ONSET (string or cell of strings, default '') a csv onset file
 %
 % OUT (string, default [pwd]) The full path for output file map.
 %
@@ -43,18 +42,34 @@ in = psom_struct_defaults( in , ...
     { 'fmri' , 'onset'}, ...
     {  NaN    , NaN });
 
+if ischar(in.fmri)
+    in.fmri = {in.fmri};
+end
+
+if ischar(in.onset) 
+    in.onset = {in.onset};
+end
+
+if length(in.onset)==1
+    in.onset = repmat(in.onset,[length(in.fmri) 1]);
+end
+
+if length(in.fmri)~=length(in.onset)
+    error('IN.FMRI should have as many entries as IN.ONSET')
+end
+
 %% Options
 opt = psom_struct_defaults ( opt , ...
   {  'folder_out' , 'glm_test' , 'flag_verbose' , 'flag_test' }, ...
   {  pwd          , 'ttest'    , true           , false         });
 folder_out = niak_full_path(opt.folder_out);
 
-%% Red the onset file
-file_onset = in.onset;
+%% Read the onset file
+file_onset = in.onset{1};
 if opt.flag_verbose
     fprintf('Read the onset file %s ...\n',file_onset);
 end
-[tab,lx,ly] = niak_read_csv(file_onset);
+[tab,lx,ly] = niak_read_csv(file_onset{1});
 
 %% Reorganize the onsets using numerical IDs for the conditions
 [list_event,tmp,all_event]  = unique(lx);
@@ -73,27 +88,52 @@ if opt.flag_test
     return
 end
 
-%% Now read an fMRI dataset
-if opt.flag_verbose
-    fprintf('Read an fMRI volume %s...\n',in.fmri)
+%% Loop over runs
+for rr = 1:length(in.fmri)
+    if (rr>1)
+        if opt.flag_verbose
+            fprintf('Read the onset file %s ...\n',file_onset);
+        end
+        [tab,lx,ly] = niak_read_csv(file_onset{rr});
+        %% Reorganize the onsets using numerical IDs for the conditions
+        [list_event,tmp,all_event]  = unique(lx);
+        opt_m.events = [all_event(:) tab];
+    end
+    
+    %% Now read an fMRI dataset
+    if opt.flag_verbose
+        fprintf('Read an fMRI volume %s...\n',in.fmri{rr})
+    end
+    [hdr,vol] = niak_read_vol(in.fmri{1});
+    if rr == 1
+        mask = niak_mask_brain(vol);
+    end
+    tseries = niak_vol2tseries(vol,mask);
+    tseries = tseries(~hdr.extra.mask_scrubbing,:);
+    tseries = niak_normalize_tseries(tseries,'perc');
+    if rr == 1
+        glm.y = tseries;
+    else
+        glm.y = [glm.y ; tseries];
+    end
+    
+    %% Build the model
+    if opt.flag_verbose
+        fprintf('Generate the model...\n')
+    end
+    opt_m.frame_times = hdr.extra.time_frames;
+    x_cache =  niak_fmridesign(opt_m);
+    reg = ones(size(vol,4),1);
+    for ee = 1:length(list_event)
+        reg = [reg x_cache.x(:,ee,1) x_cache.x(:,ee,2)];
+    end
+    reg = reg(~hdr.extra.mask_scrubbing,:);
+    if rr == 1
+        glm.x = reg;
+    else 
+        glm.x = [glm.x ; reg];
+    end
 end
-[hdr,vol] = niak_read_vol(in.fmri);
-mask = niak_mask_brain(vol);
-glm.y = niak_vol2tseries(vol,mask);
-glm.y = glm.y(~hdr.extra.mask_scrubbing,:);
-glm.y = niak_normalize_tseries(glm.y,'perc');
-
-%% Build the model
-if opt.flag_verbose
-    fprintf('Generate the model...\n')
-end
-opt_m.frame_times = hdr.extra.time_frames;
-x_cache =  niak_fmridesign(opt_m);
-glm.x = ones(size(vol,4),1);
-for ee = 1:length(list_event)
-    glm.x = [glm.x x_cache.x(:,ee,1) x_cache.x(:,ee,2)];
-end
-glm.x = glm.x(~hdr.extra.mask_scrubbing,:);
 
 %% Loop on the maps
 if opt.flag_verbose

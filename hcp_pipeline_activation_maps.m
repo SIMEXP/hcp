746ea358a7502948e11a3e9a805303573db26457
yassinebha <yanamarji@gmail.com>
2017-01-20 16:36:10 -0500

    spm maps from each run WinP

--- hcp_pipeline_activation_maps.m
+++ hcp_pipeline_activation_maps.m
@@ -67,9 +67,8 @@ opt = psom_struct_defaults(opt,list_fields,list_defaults);
 folder_out = niak_full_path(opt.folder_out);
 opt.psom.path_logs = [folder_out 'logs' filesep];
 
-% Re-organize inputs
-[files_tseries,list_subject] = sub_input(files_in);
-
+% list subject
+list_subject = fieldnames(files_in.fmri);
 %loop over subject and create spm maps jobs
 pipeline = struct();
 for num_s = 1:length(list_subject)
@@ -77,44 +76,60 @@ for num_s = 1:length(list_subject)
     subject = list_subject{num_s};
     list_session = fieldnames(files_in.fmri.(subject));
     % Session
-    for num_sess = 1:length(list_session)
-        session_name = list_session{num_sess};
-        list_run = fieldnames(files_in.fmri.(subject).(session_name));
-        % Run
-        for num_run=1:length(list_run)
-            run_name = list_run{num_run};
-            name_job = sprintf('spm_%s_%s_%s',subject,session_name,run_name);
-            in.fmri  = files_in.fmri.(subject).(session_name).(run_name);
-            in.onset = files_in.onset.(subject).(session_name).(run_name);
-            jopt.folder_out = [folder_out 'spm_maps' filesep subject filesep run_name ];
-            pipeline = psom_add_job(pipeline,name_job,'hcp_brick_fmridesign',in,struct,jopt);
-        end
+    session_name = list_session{1};
+    list_run = fieldnames(files_in.fmri.(subject).(session_name));
+    if length(list_run) > 1
+        % spm maps for all runs
+        name_job = sprintf('spm_%s_all_runs',subject);
+        in.fmri  = struct2cell(files_in.fmri.(subject).(session_name));
+        in.onset = struct2cell(files_in.onset.(subject).(session_name));
+        jopt.folder_out = [folder_out 'spm_maps' filesep subject filesep 'all_runs' ];
+        pipeline = psom_add_job(pipeline,name_job,'hcp_brick_fmridesign',in,struct,jopt);
+    end
+    % spm maps for each Run
+    for num_run=1:length(list_run)
+        clear in out jopt
+        run_name = list_run{num_run};
+        name_job = sprintf('spm_%s_%s',subject,run_name);
+        in.fmri  = files_in.fmri.(subject).(session_name).(run_name);
+        in.onset = files_in.onset.(subject).(session_name).(run_name);
+        jopt.folder_out = [folder_out 'spm_maps' filesep subject filesep run_name ];
+        pipeline = psom_add_job(pipeline,name_job,'hcp_brick_fmridesign',in,struct,jopt);
     end
 end
+
+% Mean t-maps
+%% all runs
+for num_s = 1:length(list_subject)
+    clear in out jopt
+    subject = list_subject{num_s};
+    list_session = fieldnames(files_in.fmri.(subject));
+    % Session
+    session_name = list_session{1};
+    list_run = fieldnames(files_in.fmri.(subject).(session_name));
+    name_job = sprintf('spm_%s_all_runs',subject);
+    in.fmri  = struct2cell(files_in.fmri.(subject).(session_name));
+    in.onset = struct2cell(files_in.onset.(subject).(session_name));
+    jopt.folder_out = [folder_out 'spm_maps' filesep subject filesep 'all_runs' ];
+    pipeline = psom_add_job(pipeline,name_job,'hcp_brick_fmridesign',in,struct,jopt);
+
+    % %%% this section sould be a brick
+    % for ss = 1 : length(list_subject)
+    %     [~,mask]= niak_read_vol('mean_func_mask.mnc.gz');
+    %     [hdr,vol]= niak_read_vol('spm_map.mnc.gz');
+    %     x = zeros(length(list_subject),sum(mask(:)));
+    %     x(ss,:) = vol(mask > 0);
+    % end
+    % ttest_x = niak_ttest(x);
+    % hdr.file_name = out.(event_name);
+    % niak_write_vol(hdr,niak_tseries2vol(ttest_x(:)',mask));
+
+end
+
+
+
 
 %% Run the pipeline
 if ~opt.flag_test
     psom_run_pipeline(pipeline,opt.psom);
-end
-
-
-%%%%%%%%%%%%%%%%%%
-%% SUBFUNCTIONS %%
-%%%%%%%%%%%%%%%%%%
-function [files_tseries,list_subject] = sub_input(files_in);
-files_tseries = files_in.fmri;
-list_subject = fieldnames(files_tseries);
-for num_s = 1:length(list_subject)
-    subject  = list_subject{num_s};
-    files_subject = files_tseries.(subject);
-    list_session = fieldnames(files_subject);
-    nb_data = 1;
-    for num_sess = 1:length(list_session)
-        list_run = fieldnames(files_subject.(list_session{num_sess}));
-        for num_r = 1:length(list_run)
-             files_tmp{nb_data} = files_subject.(list_session{num_sess}).(list_run{num_r});
-             nb_data = nb_data + 1;
-        end
-    end
-    files_tseries.(subject) = files_tmp;
 end

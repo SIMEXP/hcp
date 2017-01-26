4a9302eef3747baf9b918793264a3946728f6aae
yassinebha <yanamarji@gmail.com>
2017-01-26 16:44:30 -0500

    motor individual onset

--- hcp_run_spm_motor.m
+++ hcp_run_spm_motor.m
@@ -8,7 +8,40 @@ opt_grab.filter.run = {'motRL'};
 opt_grab.min_nb_vol = 100;
 files_grab = niak_grab_fmri_preprocess([root_path 'fmri_preprocess_all_tasks_niak-fix-scrub_900R'], opt_grab);
 files_in.fmri =  files_grab.fmri;
-files_in.onset = '/sb/home/yassinebha/hcp_motor_RL_onset.csv'
+files_in.mask = '/gs/project/gsf-624-aa/HCP/fmri_preprocess_all_tasks_niak-fix-scrub_900R/quality_control/group_coregistration/func_mask_group_stereonl.mnc.gz';
+% grab individul onset files
+list_subj=fieldnames(files_in.fmri);
+for ss=1:length(list_subj)
+    subject = list_subj{ss};
+    list_run = fieldnames(files_in.fmri.(subject).sess1);
+    for rr=1:length(list_run)
+        subj_run = list_run{rr};
+        switch subj_run
+               case 'motLR'
+                   subj_onset = [root_path filesep 'fmri_preprocess_all_tasks_niak-fix-scrub_900R/EVs' filesep subject filesep 'MOTOR_LR/MOTOR_LR_spm_onset.csv' ];
+               case 'motRL'
+                   subj_onset = [root_path filesep 'fmri_preprocess_all_tasks_niak-fix-scrub_900R/EVs' filesep subject filesep 'MOTOR_RL/MOTOR_RL_spm_onset.csv' ];
+        end
+        if ~exist(subj_onset)
+           warning('Subject %s run %s has no Onset file, this run will be discarded',subject,subj_run)
+           files_in.fmri.(subject).sess1 = rmfield(files_in.fmri.(subject).sess1,subj_run);
+        else
+        files_in.onset.(subject).sess1.(subj_run) = subj_onset;
+        end
+    end
+    if ~isfield( files_in.fmri.(subject).sess1,'motLR') && ~isfield( files_in.fmri.(subject).sess1,'motRL')
+       warning('Subject %s is discarded, has no onset for both runs',subject)
+       files_in.fmri = rmfield(files_in.fmri,subject);
+    end
+end
+% set pipeline options
+opt.folder_out = [root_path 'hcp_motor_activation_maps_' date];
+[pipeline,opt] = hcp_pipeline_activation_maps(files_in,opt);
+
+
+
+
+
 % set pipeline options
 opt.folder_out = [root_path 'activation_maps'];
 [pipeline,opt] = hcp_pipeline_activation_maps(files_in,opt);

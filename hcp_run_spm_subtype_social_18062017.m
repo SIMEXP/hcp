e27ee62849d70dfc90afff881d759c7afbc86d0b
yassinebha <yanamarji@gmail.com>
2017-08-21 08:13:39 -0400

    rerun social, found bu in the contrast selection

--- hcp_run_spm_subtype_social_18062017.m
+++ hcp_run_spm_subtype_social_18062017.m
@@ -65,9 +65,9 @@ file_pheno = [path_root 'pheno/hcp_pheno_social.csv'];
 
 
 ## Grab spm maps
-path_spm = [path_root 'hcp_social_activation_maps_17-Jun-2017'];
+path_spm = [path_root 'hcp_social_activation_maps_19-Aug-2017'];
 opt_spm.run_name = 'all_runs';
-opt_spm.list_trial = {'mental','mental_vs_rnd'};
+opt_spm.list_trial = {'mental','contrast_mental_vs_rnd'};
 files_spm = hcp_grab_spm_maps(path_spm,opt_spm);
 files_in.data = files_spm.spm_map;
 
